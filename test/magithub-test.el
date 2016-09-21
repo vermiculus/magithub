4f71a9d8687a6d3cc2d3c85acc4ba46c3df13a75
Sean Allred <code@seanallred.com>
2016-09-21 00:16:00 -0500

    Try something

--- test/magithub-test.el
+++ test/magithub-test.el
@@ -8,6 +8,8 @@
 
 (require 'ert)
 
+(add-to-list 'load-path ".")
+
 (ert-deftest magithub-test-compile ()
   (should (byte-compile-file "magithub.el"))
   (should (byte-compile-file "magithub-issue.el")))
