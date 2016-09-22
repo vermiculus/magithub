f33b89bd376f6ab863d65ac7bc1ae63065f86229
Sean Allred <code@seanallred.com>
2016-09-21 23:21:48 -0500

    Fix compile issue

--- test/magithub-test.el
+++ test/magithub-test.el
@@ -11,7 +11,9 @@
 (add-to-list 'load-path ".")
 
 (ert-deftest magithub-test-compile ()
-  (should (byte-compile-file "magithub.el"))
-  (should (byte-compile-file "magithub-issue.el")))
+  (should (byte-compile-file "magithub-core.el"))
+  (should (byte-compile-file "magithub-issue.el"))
+  (should (byte-compile-file "magithub-ci.el"))
+  (should (byte-compile-file "magithub.el")))
 
 ;;; magithub-test.el ends here
