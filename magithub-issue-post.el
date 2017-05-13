ea6dbfb014e845a9de05293c1a7b8d702bf92040
Sean Allred <code@seanallred.com>
2017-05-12 23:10:17 -0500

    Support posting pull requests

--- magithub-issue-post.el
+++ magithub-issue-post.el
@@ -12,12 +12,23 @@
   "Magithub Issue Edit"
   "Major mode for editing GitHub issues.")
 
-(defvar-local magithub-issue--widget-title nil)
-(defvar-local magithub-issue--widget-labels nil)
-(defvar-local magithub-issue--widget-body nil)
+(defvar-local magithub-issue--extra-data nil)
+(defvar-local magithub-issue--widgets nil
+  "Alist of symbols to widgets.
+Special property `:type' is either `issue' or `pull-request' and
+determines the behavior of `magithub-issue-wsubmit'.")
+(defun magithub-issue--widget-get (key)
+  (alist-get key magithub-issue--widgets))
+(defun magithub-issue--widget-value (key)
+  (widget-value (magithub-issue--widget-get key)))
 
-(define-key magithub-issue-post-mode-map "b" #'magithub-issue-w-jump-to-body)
-(define-key magithub-issue-edit-mode-map (kbd "C-c C-c C-c") #'kill-buffer-and-window)
+(let ((m magithub-issue-post-mode-map))
+  (define-key m "b" #'magithub-issue-w-jump-to-body)
+  (define-key m (kbd "C-c RET") #'magithub-issue-wsubmit)
+  (define-key m (kbd "C-c C-k") #'magithub-issue-wcancel))
+
+(let ((m magithub-issue-edit-mode-map))
+  (define-key m (kbd "C-c C-c C-c") #'kill-buffer-and-window))
 
 (defvar magithub-issue-widget-map
   (let ((m (copy-keymap widget-keymap)))
@@ -53,14 +64,27 @@
 
 (defun magithub-issue--w-start-of-body ()
   (save-excursion
-    (goto-char (plist-get (cdr magithub-issue--widget-body) :from))
+    (goto-char (widget-get (magithub-issue--widget-get 'body) :from))
     (forward-line)
     (point)))
 (defun magithub-issue--w-end-of-body ()
   (save-excursion
-    (goto-char (plist-get (cdr magithub-issue--widget-body) :to))
+    (goto-char (widget-get (magithub-issue--widget-get 'body) :to))
     (backward-char 3)
     (point)))
+(defun magithub-issue-w-next-widget-dwim ()
+  (interactive)
+  (ignore-errors
+    (let ((start (magithub-issue--w-start-of-body))
+          (end   (magithub-issue--w-end-of-body))
+          (keys  (kbd (substitute-command-keys
+                       "\\[magithub-issue-w-next-widget-dwim]"))))
+      (if (or (<= (point) start) (<= end (point)))
+          (call-interactively #'widget-forward)
+        (when-let ((func (with-temp-buffer
+                           (magithub-issue-edit-mode)
+                           (key-binding keys))))
+          (call-interactively func))))))
 
 (defvar magithub-issue-title-map
   (let ((m (copy-keymap magithub-issue-widget-map)))
@@ -69,8 +93,8 @@
 
 (defun magithub-issue-edit ()
   (interactive)
-  (let ((start (save-excursion (goto-char (magithub-issue--w-start-of-body)) (point)))
-        (end   (save-excursion (goto-char (magithub-issue--w-end-of-body))   (point))))
+  (let ((start (magithub-issue--w-start-of-body))
+        (end   (magithub-issue--w-end-of-body)))
     (with-current-buffer (clone-indirect-buffer "*mgh edit*" t)
       (remove-overlays)
       (narrow-to-region start end)
@@ -93,65 +117,138 @@
   :format "%t:\n%v\n\n"
   :inline nil)
 
-(defun magithub-issue--new-form (repo issue)
-  (when (alist-get 'body issue)
-    (error "issue.body not yet supported; see https://emacs.stackexchange.com/q/32674/2264"))
+(defun magithub-issue--new-form (repo issue
+                                      buffer-name
+                                      header
+                                      show-labels-p
+                                      submit-caption
+                                      submit-function
+                                      cancel-caption
+                                      cancel-function)
+  "Opens a new widget-based form for issue/PR submission.
+
+REPO should be a full repository object.
+
+ISSUE should be an issue object.  The `title' and `labels'
+properties are respected and prepopulate the form."
   (let-alist `((repo . ,repo) (issue . ,issue))
-    (with-current-buffer (generate-new-buffer "*magithub issue*")
+    (with-current-buffer (generate-new-buffer buffer-name)
       (magithub-issue-post-mode)
-      (setq header-line-format (concat "Creating an issue for " .repo.full_name))
+      (setq header-line-format
+            (substitute-command-keys
+             (s-join " | " (list header
+                                 "submit: \\[magithub-issue-wsubmit]"
+                                 "cancel: \\[magithub-issue-wcancel]"))))
+      (push
+       (cons 'title (widget-create 'magithub-issue-title
+                                   :size 76
+                                   .issue.title))
+       magithub-issue--widgets)
 
-      (setq magithub-issue--widget-title
-            (widget-create 'magithub-issue-title :size 76 .issue.title))
+      (when show-labels-p
+        (push (cons 'labels
+                    (let ((w (apply #'widget-create 'magithub-issue-labels
+                                    (mapcar (lambda (label) `(item ,(alist-get 'name label)))
+                                            (magithub-label-list)))))
+                      (widget-value-set w (magithub-get-in-all '(name) .issue.labels))
+                      w))
+              magithub-issue--widgets))
 
-      (when .repo.permissions.push
-        (setq magithub-issue--widget-labels
-              (apply #'widget-create 'magithub-issue-labels
-                     (mapcar (lambda (label) `(item ,(alist-get 'name label)))
-                             (magithub-label-list))))
-        (widget-value-set magithub-issue--widget-labels (magithub-get-in-all '(name) .issue.labels)))
-
-      (setq magithub-issue--widget-body (widget-create 'magithub-issue-text))
+      (push (cons 'body (widget-create 'magithub-issue-text))
+            magithub-issue--widgets)
 
       (widget-insert "\n")
-      (widget-create 'push-button :notify #'magithub-issue-wsubmit "Create new issue")
+      (widget-create 'push-button :notify submit-function submit-caption)
       (widget-insert "  ")
-      (widget-create 'push-button :notify #'magithub-issue-wsave "Save draft")
-      (widget-insert "  ")
-      (widget-create 'push-button :notify #'magithub-issue-wcancel "Cancel")
+      (widget-create 'push-button :notify cancel-function cancel-caption)
       (widget-insert "\n")
       (use-local-map magithub-issue-widget-map)
       (widget-setup)
       (magithub-issue-w-jump-to-body)
-      (current-buffer)))))
+      (current-buffer))))
 
 (defun magithub-issue-new (repo title labels)
-  (interactive (list (magithub-source-repo nil t) nil nil))
-  ;;(setcdr (assq 'push (cdr (assq 'permissions repo))) nil)
-  (when (called-interactively-p)
-    (let-alist repo
-      (setq title (read-string (format "Issue title (%s): " .full_name))
-            labels (when .permissions.push
-                     (magithub-label-read-labels "Labels: ")))))
-  (switch-to-buffer-other-window
-   (magithub-issue--new-form repo `((title . ,title) (labels . ,labels)))))
+  (interactive
+   (let-alist (setq repo (magithub-source-repo nil t))
+     (list repo
+           (read-string (format "Issue title (%s): " .full_name))
+           (when .permissions.push
+             (magithub-label-read-labels "Labels: ")))))
 
-(defun magithub-issue-wsubmit (&rest _)
+  (let-alist repo
+    (switch-to-buffer-other-window
+     (magithub-issue--new-form
+      repo `((title . ,title) (labels . ,labels))
+      "*magithub-issue*"
+      (format "Creating an issue for %s" .full_name)
+      .permissions.push
+      "Create new issue"
+      #'magithub-issue-wsubmit-issue
+      "Cancel"
+      #'magithub-issue-wcancel))))
+
+(defun magithub-pull-request-new (repo title base head)
+  (interactive
+   (let-alist (setq repo (magithub-source-repo))
+     (list repo
+           (read-string (format "Pull request title (%s/%s): " .owner.login .name))
+           (magit-read-branch "Base branch" (magit-get-upstream-branch))
+           (format "%s:%s" (ghub--username) (magit-read-branch "Head")))))
+
+  (let-alist repo
+    (with-current-buffer
+        (magithub-issue--new-form
+         repo `((title . ,title))
+         "*magithub-pull-request*"
+         (format "PR %s/%s (%s->%s)" .owner.login .name head base)
+         nil
+         "Submit new pull request"
+         #'magithub-issue-wsubmit-pull-request
+         "Cancel"
+         #'magithub-issue-wcancel)
+      (setq magithub-issue--extra-data
+            `((base . ,base) (head . ,head)))
+      (switch-to-buffer-other-window (current-buffer)))))
+
+(defun magithub-issue-wsubmit-issue (&rest _)
   (interactive)
-  (when (yes-or-no-p "Are you sure you want to submit this issue? ")
-    (let ((issue `((title . ,(s-trim (widget-value magithub-issue--widget-title)))
-                   (labels . ,(widget-value magithub-issue--widget-labels))
-                   (body . ,(s-trim (widget-value magithub-issue--widget-body))))))
+
+  (let ((issue `((title  . ,(s-trim (magithub-issue--widget-value 'title)))
+                 (labels . ,(magithub-issue--widget-value 'labels))
+                 (body   . ,(s-trim (magithub-issue--widget-value 'body))))))
+    (when (s-blank-p (alist-get 'title issue))
+      (user-error "Title is required"))
+    (when (yes-or-no-p "Are you sure you want to submit this issue? ")
       (magithub-issue-browse
-       (ghubp-post-repos-owner-repo-issues (magithub-source-repo) issue)))
-    (kill-buffer-and-window)))
-(defun magithub-issue-wsave (&rest _)
-  (error "feature not implemented"))
+       (ghubp-post-repos-owner-repo-issues (magithub-source-repo) issue))
+      (kill-buffer-and-window))))
+(defun magithub-issue-wsubmit-pull-request (&rest _)
+  (interactive)
+  (let ((pull-request `((title  . ,(s-trim (magithub-issue--widget-value 'title)))
+                        (body   . ,(s-trim (magithub-issue--widget-value 'body)))
+                        (base   . ,(alist-get 'base magithub-issue--extra-data))
+                        (head   . ,(alist-get 'head magithub-issue--extra-data)))))
+    (when (s-blank-p (alist-get 'title pull-request))
+      (user-error "Title is required"))
+    (when (yes-or-no-p "Are you sure you want to submit this pull request? ")
+      (when (y-or-n-p "Allow maintainers to modify this pull request? ")
+        (push (cons 'maintainer_can_modify t) pull-request))
+      (magithub-issue-browse
+       (ghubp-post-repos-owner-repo-pulls (magithub-source-repo) pull-request))
+      (kill-buffer-and-window))))
+
 (defun magithub-issue-wcancel (&rest _)
-  (when (y-or-n-p "Save draft? ")
-    (magithub-issue-wsave))
-  (kill-buffer-and-window))
+  (interactive)
+  ;; It'd be nice to have a sort of kill ring for cancelled issues
+  (when (yes-or-no-p "You will lose this buffer completely; are you sure? ")
+    (kill-buffer-and-window)))
+
+(advice-add #'ghub--request
+            :around
+            (lambda (oldfun &rest args)
+              (if (memq 'dry-api magithub-debug-mode)
+                  (message "ghub-request(%S)" args)
+                (apply oldfun args)))
+            '(:name debug))
 
 (provide 'magithub-issue-widget)
-
-;;; (defun magithub--url->repo (&rest _) '((owner (login . "vermiculus")) (name . "my-new-repository")))
