062791688dc29281dec7ca490df0abe03e66e0ee
Sean Allred <code@seanallred.com>
2017-05-15 23:05:49 -0500

    Remove dead test code
    As of e69a79a37e04b4cf76a3c4fc349ad9cf87401519, this should no longer
    be necessary.

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,9 +1,4 @@
 ;;; Allow loading package files
 (add-to-list 'load-path default-directory)
 
-;;; Avoid awkward situations where Travis has to answer a question
-;;; https://travis-ci.org/vermiculus/magithub/jobs/231915489#L880
-(let ((d (expand-file-name "magithub" user-emacs-directory)))
-  (or (file-exists-p d) (mkdir d)))
-
 (load-file "./test/magithub-test-compile.el")
