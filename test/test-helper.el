6c1ec030226782c7ad3cbfc204b54f22a97cc064
Sean Allred <code@seanallred.com>
2017-07-30 09:18:31 -0500

    Don't authenticate tests

--- test/test-helper.el
+++ test/test-helper.el
@@ -2,3 +2,7 @@
 (add-to-list 'load-path default-directory)
 
 (load-file "./test/magithub-test-compile.el")
+
+;;; Don't authenticate these requests; no known way to keep that
+;;; authentication secret
+(setq ghub-authenticate nil)
