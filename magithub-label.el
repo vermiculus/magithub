f305f84fa60cd2d469eac60c808fa877ac9b08b0
Sean Allred <code@seanallred.com>
2017-10-16 21:34:20 -0500

    Add functionality to label-type sections

--- magithub-label.el
+++ magithub-label.el
@@ -1,6 +1,14 @@
 (require 'ghub+)
 (require 'magithub-core)
 
+(defvar magit-magithub-label-section-map
+  (let ((m (make-sparse-keymap)))
+    (define-key m [remap magit-visit-thing]  #'magithub-label-visit)
+    (define-key m [remap magit-delete-thing] #'magithub-label-remove)
+    (define-key m (kbd "a") #'magithub-label-add)
+    m)
+  "Keymap for label sections.")
+
 (defun magithub-label-list ()
   "Return a list of issue and pull-request labels."
   (magithub-cache :label
@@ -75,5 +83,43 @@ from `magithub-label'.  Customize that to affect all labels."
                              "Auto-saved by `magithub-label-color-replace'"))
   (when (derived-mode-p 'magit-status-mode)
     (magit-refresh)))
+
+(defun magithub-label-visit (label)
+  "Visit LABEL."
+  (interactive (list (magit-section-value (magit-current-section))))
+  (magithub-label-browse label))
+
+(defun magithub-label-remove (issue label)
+  "From ISSUE, remove LABEL."
+  (interactive (let ((s (magit-current-section)))
+                 (list (magit-section-parent-value s)
+                       (magit-section-value s))))
+  (if (and issue label)
+      (let-alist `((label . ,label)
+                   (issue . ,issue)
+                   (repo . ,(magithub-issue-repo issue)))
+        (if (yes-or-no-p (format "Remove label %S from this issue? " .label.name))
+            (prog1 (ghubp-delete-repos-owner-repo-issues-number-labels-name
+                    (magithub-issue-repo issue) issue label)
+              (magithub-cache-without-cache :issues
+                (magit-refresh-buffer)))
+          (user-error "Aborted")))
+    (user-error "No label here")))
+
+(defun magithub-label-add (issue labels)
+  "To ISSUE, add LABELS."
+  (interactive (list (magit-section-parent-value (magit-current-section))
+                     (magithub-label-read-labels "Add labels: ")))
+  (if (not (and issue labels))
+      (user-error "No issue/labels")
+    (if (yes-or-no-p (format "Add {%s} to %s#%s? "
+                             (s-join "," (ghubp-get-in-all '(name) labels))
+                             (magithub-repo-name (magithub-issue-repo issue))
+                             (alist-get 'number issue)))
+        (prog1 (ghubp-post-repos-owner-repo-issues-number-labels
+                (magithub-issue-repo issue) issue labels)
+          (magithub-cache-without-cache :issues
+            (magit-refresh)))
+      (user-error "Aborted"))))
 
 (provide 'magithub-label)
