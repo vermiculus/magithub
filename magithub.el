db805e9e4545972dda2259ad3a76a5e09870126a
Matteo F. Vescovi <mfvescovi@users.noreply.github.com>
2017-11-08 18:11:16 +0100

    Bump package version
    Again! ;-)

--- magithub.el
+++ magithub.el
@@ -6,7 +6,7 @@
 ;; Keywords: git, tools, vc
 ;; Homepage: https://github.com/vermiculus/magithub
 ;; Package-Requires: ((emacs "25") (magit "2.8") (s "20170428.1026") (ghub+ "0.2") (git-commit "2.8") (markdown-mode "2.3"))
-;; Package-Version: 0.1.3
+;; Package-Version: 0.1.4
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
