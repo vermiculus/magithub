7dc101f6bb87ddd17422ad1524b56118d0dc16dc
Sean Allred <code@seanallred.com>
2017-12-23 10:07:43 -0600

    Visit CI status with `w' as well

--- magithub-ci.el
+++ magithub-ci.el
@@ -204,7 +204,9 @@ remote counterpart."
 
 (defvar magit-magithub-ci-status-section-map
   (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map magithub-map)
     (define-key map [remap magit-visit-thing] #'magithub-ci-visit)
+    (define-key map [remap magithub-browse-thing] #'magithub-ci-visit)
     (define-key map [remap magit-refresh] #'magithub-ci-refresh)
     map)
   "Keymap for `magithub-ci-status' header section.")
