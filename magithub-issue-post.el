vermiculus/magithub

commit 2a372fbc16a8f6f033be7da951c7ec0b2ffd15be

Dump widget framework in favor of magithub-edit-mode

magithub-issue-post.el
@@ -1,174 +1,56 @@
 (require 'magithub-core)
 (require 'magithub-issue)
 (require 'magithub-label)
-
-(require 'widget)
-(eval-and-compile
-  (require 'wid-edit))
+(require 'magithub-edit-mode)
 
 (declare-function magithub-issue-view "magithub-issue-view.el" (issue))
 
-(define-derived-mode magithub-issue-post-mode nil
-  "Magithub Issue Post"
-  "Major mode for posting Github issues and pull requests.")
-
 (defvar-local magithub-issue--extra-data nil)
-(defvar-local magithub-issue--widgets nil
-  "Alist of symbols to widgets.")
-(defun magithub-issue--widget-get (key)
-  (alist-get key magithub-issue--widgets))
-(defun magithub-issue--widget-value (key)
-  (widget-value (magithub-issue--widget-get key)))
 
-(setq magithub-issue-post-mode-map
-      (let ((m (copy-keymap widget-keymap)))
-        (define-key m (kbd "C-c RET") #'magithub-issue-wsubmit)
-        (define-key m (kbd "C-c C-k") #'magithub-issue-wcancel)
-        (define-key m "b" #'magithub-issue-w-jump-to-body)
-        m))
-
-(add-hook 'magithub-issue-post-mode-hook
-          #'magithub-bug-reference-mode-on)
-
-(defun magithub-issue-w-beginning-of-buffer-dwim ()
+(defun magithub-issue-post-submit ()
   (interactive)
-  (let ((start-of-body (magithub-issue--w-start-of-body)))
-    (goto-char
-     (if (= (point) start-of-body)
-         (point-min)
-       start-of-body))))
-(defun magithub-issue-w-end-of-buffer-dwim ()
-  (interactive)
-  (let ((end-of-body (magithub-issue--w-end-of-body)))
-    (goto-char
-     (if (= (point) end-of-body)
-         (point-max)
-       end-of-body))))
-(defun magithub-issue-w-jump-to-body ()
-  (interactive)
-  (if (or (not (widget-at))
-          (eq 'checkbox (widget-type (widget-at))))
-      (goto-char (magithub-issue--w-start-of-body))
-    (call-interactively #'self-insert-command)))
+  (let ((issue (magithub-issue-post--parse-buffer))
+        (repo (magithub-repo)))
+    (when (s-blank-p (alist-get 'title issue))
+      (user-error "Title is required"))
+    (when (magithub-repo-push-p repo)
+      (when-let ((issue-labels (magithub-label-read-labels "Labels: ")))
+        (push (cons 'labels issue-labels) issue)))
+    (when (yes-or-no-p "Are you sure you want to submit this issue? ")
+      (let ((issue (magithub-request
+                    (ghubp-post-repos-owner-repo-issues repo issue))))
+        (magithub-issue-view issue)))))
 
-(defun magithub-issue--w-start-of-body ()
-  (save-excursion
-    (goto-char (widget-get (magithub-issue--widget-get 'body) :from))
-    (forward-line)
-    (point)))
-(defun magithub-issue--w-end-of-body ()
-  (save-excursion
-    (goto-char (widget-get (magithub-issue--widget-get 'body) :to))
-    (backward-char 3)
-    (point)))
-(defun magithub-issue-w-next-widget-dwim ()
-  (interactive)
-  (ignore-errors
-    (let ((start (magithub-issue--w-start-of-body))
-          (end   (magithub-issue--w-end-of-body))
-          (keys  (kbd (substitute-command-keys
-                       "\\[magithub-issue-w-next-widget-dwim]"))))
-      (if (or (<= (point) start) (<= end (point)))
-          (call-interactively #'widget-forward)
-        (when-let ((func (with-temp-buffer
-                           (key-binding keys))))
-          (call-interactively func))))))
+(defun magithub-issue-post--parse-buffer ()
+  (let (split)
+    (save-restriction
+      (widen)
+      (save-excursion
+        (goto-char (point-min))
+        (forward-line)
+        (setq split (point)))
+      `((title . ,(s-trim (buffer-substring-no-properties (point-min) (1- split))))
+        (body  . ,(s-trim (buffer-substring-no-properties split (point-max))))))))
 
-(define-widget 'magithub-issue-title 'editable-field
-  "Issue / pull-request title entry"
-  :tag "Title"
-  :format "%t: %v \n\n")
-(define-widget 'magithub-issue-labels 'checklist
-  "Tag entry"
-  :greedy t
-  :tag "Labels"
-  :format "%t:\n%v \n\n")
-(define-widget 'magithub-issue-text 'text
-  "Issue / pull-request body entry"
-  :tag "Body"
-  :format "%t:\n%v\n\n"
-  :inline nil)
+(defun magithub-issue-new (repo)
+  (interactive (list (magithub-repo)))
+  (with-current-buffer
+      (magithub-edit-new (format "*magithub-issue: %s*" (magithub-repo-name repo))
+        :header (format "Creating an issue for %s" (alist-get 'full_name repo))
+        :submit #'magithub-issue-post-submit
+        :file (expand-file-name "new-issue-draft"
+                                (magithub-repo-data-dir repo))
+        :template (magithub-issue--template-text "ISSUE_TEMPLATE"))
+    (font-lock-add-keywords nil `((,(rx bos (group (*? any)) eol) 1
+                                   'magithub-issue-title-edit t)))
+    (magithub-bug-reference-mode-on)
+    (magit-display-buffer (current-buffer))))
 
-(defun magithub-issue--new-form (repo issue
-                                      buffer-name
-                                      header
-                                      show-labels-p
-                                      submit-caption
-                                      submit-function
-                                      cancel-caption
-                                      cancel-function)
-  "Opens a new widget-based form for issue/PR submission.
-
-REPO should be a full repository object.
-
-ISSUE should be an issue object.  The `title' and `labels'
-properties are respected and prepopulate the form."
-  (let-alist `((repo . ,repo) (issue . ,issue))
-    (with-current-buffer (generate-new-buffer buffer-name)
-      (magithub-issue-post-mode)
-      (setq header-line-format
-            (substitute-command-keys
-             (s-join " | " (list header
-                                 "submit: \\[magithub-issue-wsubmit]"
-                                 "cancel: \\[magithub-issue-wcancel]"))))
-      (push
-       (cons 'title (widget-create 'magithub-issue-title
-                                   :size 76
-                                   .issue.title))
-       magithub-issue--widgets)
-
-      (when show-labels-p
-        (push (cons 'labels
-                    (let ((w (apply #'widget-create 'magithub-issue-labels
-                                    (mapcar (lambda (label) `(item ,(alist-get 'name label)))
-                                            (magithub-label-list)))))
-                      (widget-value-set w (ghubp-get-in-all '(name) .issue.labels))
-                      w))
-              magithub-issue--widgets))
-
-      (push (cons 'body (widget-create 'magithub-issue-text))
-            magithub-issue--widgets)
-
-      (widget-insert "\n")
-      (widget-create 'push-button :notify submit-function submit-caption)
-      (widget-insert "  ")
-      (widget-create 'push-button :notify cancel-function cancel-caption)
-      (widget-insert "\n")
-      (widget-setup)
-      (magithub-issue-w-jump-to-body)
-      ;; GFM-mode doesn't handle line breaks just yet
-      (visual-line-mode 1)
-      (current-buffer))))
-
-(defun magithub-issue-new (repo title labels)
-  (interactive
-   (let-alist (setq repo (magithub-repo))
-     (list repo
-           (read-string (format "Issue title (%s): " .full_name))
-           (when .permissions.push
-             (magithub-label-read-labels "Labels: ")))))
-
-  (let-alist repo
-    (with-current-buffer
-        (magithub-issue--new-form
-         repo `((title . ,title) (labels . ,labels))
-         "*magithub-issue*"
-         (format "Creating an issue for %s" .full_name)
-         .permissions.push
-         "Create new issue"
-         #'magithub-issue-wsubmit-issue
-         "Cancel"
-         #'magithub-issue-wcancel)
-      (setq magithub-issue--extra-data '((kind . issue)))
-      (magithub-issue--template-insert "ISSUE_TEMPLATE")
-      (switch-to-buffer-other-window (current-buffer)))))
-
-(defun magithub-issue--template-insert (filename)
-  "Inserts template FILENAME into the issue body"
-  (save-excursion
-    (magithub-issue-w-jump-to-body)
-    (when-let ((template (magithub-issue--template-find filename)))
-      (insert-file-contents template))))
+(defun magithub-issue--template-text (template)
+  (with-temp-buffer
+    (when-let ((template (magithub-issue--template-find template)))
+      (insert-file-contents template)
+      (buffer-string))))
 
 (defun magithub-issue--template-find (filename)
   "Find an appropriate template called FILENAME and returns its absolute path.
@@ -199,99 +81,77 @@ See also URL
    nil t nil nil default))
 
 (defun magithub-pull-request-new-arguments ()
+  (unless (magit-get-push-remote)
+    (user-error "Nothing on remote yet; aborting"))
   (let* ((this-repo   (magithub-read-repo "Fork's remote (this is you!) "))
          (this-repo-owner (let-alist this-repo .owner.login))
          (parent-repo (or (alist-get 'parent this-repo) this-repo))
          (this-remote (car (magithub-repo-remotes-for-repo this-repo)))
          (on-this-remote (string= (magit-get-push-remote) this-remote))
          (base-remote (car (magithub-repo-remotes-for-repo parent-repo)))
-         (head        (magithub-remote-branches-choose
+         (head-branch (magithub-remote-branches-choose
                        "Head branch" this-remote
                        (when on-this-remote
                          (magit-get-current-branch))))
+         (user+head   (format "%s:%s" this-repo-owner head-branch))
          (base        (magithub-remote-branches-choose
                        "Base branch" base-remote
                        (when on-this-remote
-                         (magit-get-upstream-branch head))))
-         (head (if (string= this-remote base-remote)
-                   head
-                 (format "%s:%s" this-repo-owner head))))
+                         (magit-get-upstream-branch head-branch)))))
+    (when (magithub-request (ghubp-get-repos-owner-repo-pulls parent-repo nil :head user+head))
+      (user-error "A pull request on %s already exists for head \"%s\""
+                  (magithub-repo-name parent-repo)
+                  user+head))
     (unless (y-or-n-p (format "You are about to create a pull request to merge branch `%s' into %s:%s; is this what you wanted to do?"
-                              head (magithub-repo-name parent-repo) base))
+                              user+head (magithub-repo-name parent-repo) base))
       (user-error "Aborting"))
     (let-alist parent-repo
-      (list parent-repo base head
-            (read-string (format "Pull request title (%s/%s): "
-                                 .owner.login .name))))))
+      (list parent-repo base
+            (if (string= this-remote base-remote)
+                head-branch
+              user+head)))))
 
-(defun magithub-pull-request-new (repo base head title)
+(defun magithub-pull-request-new (repo base head)
   "Create a new pull request."
-  (interactive (magithub-pull-request-new-arguments))
-  (when (ghubp-get-repos-owner-repo-pulls repo nil :head head)
-    (user-error "A pull request on %s already exists for head %s"
-                (magithub-repo-name repo)
-                head))
-  (let-alist repo
-    (with-current-buffer
-        (magithub-issue--new-form
-         repo `((title . ,title))
-         "*magithub-pull-request*"
-         (format "PR %s/%s (%s->%s)" .owner.login .name head base)
-         nil
-         "Submit new pull request"
-         #'magithub-issue-wsubmit-pull-request
-         "Cancel"
-         #'magithub-issue-wcancel)
-      (setq magithub-issue--extra-data
-            `((base . ,base) (head . ,head)
-              (kind . pull-request)))
-      (magithub-issue--template-insert "PULL_REQUEST_TEMPLATE")
-      (switch-to-buffer-other-window (current-buffer)))))
+  (interactive
+   (progn
+     (unless (magit-get-push-remote)
+       (when (magit-y-or-n-p "Nothing on remote for this branch; would you like to push? ")
+         (magit-push-popup))
+       (user-error "Try again after push"))
+     (magithub-pull-request-new-arguments)))
+  (with-current-buffer
+      (magithub-edit-new (format "*magithub-pull-request: %s into %s:%s*"
+                                 head
+                                 (magithub-repo-name repo)
+                                 base)
+        :header (let-alist repo (format "PR %s/%s (%s->%s)" .owner.login .name head base))
+        :submit #'magithub-pull-request-submit
+        :file (expand-file-name "new-pull-request-draft"
+                                (magithub-repo-data-dir repo))
+        :template (magithub-issue--template-text "PULL_REQUEST_TEMPLATE"))
+    (font-lock-add-keywords nil `((,(rx bos (group (*? any)) eol) 1
+                                   'magithub-issue-title-edit t)))
+    (magithub-bug-reference-mode-on)
+    (setq magithub-issue--extra-data
+          `((base . ,base) (head . ,head)))
+    (magit-display-buffer (current-buffer))))
 
-(defun magithub-issue-wsubmit ()
+(defun magithub-pull-request-submit ()
   (interactive)
-  (call-interactively
-   (pcase (alist-get 'kind magithub-issue--extra-data)
-     ('pull-request #'magithub-issue-wsubmit-pull-request)
-     ('issue #'magithub-issue-wsubmit-issue))))
-
-(defun magithub-issue-wsubmit-issue (&rest _)
-  (interactive)
-  (let ((issue `((title  . ,(s-trim (magithub-issue--widget-value 'title)))
-                 (body   . ,(s-trim (magithub-issue--widget-value 'body)))
-                 ,@(when-let ((vlabels (ignore-errors (magithub-issue--widget-value 'labels))))
-                     `((labels . ,vlabels))))))
-    (when (s-blank-p (alist-get 'title issue))
-      (user-error "Title is required"))
-    (when (yes-or-no-p "Are you sure you want to submit this issue? ")
-      (let ((issue (magithub-request
-                    (ghubp-post-repos-owner-repo-issues (magithub-repo) issue))))
-        (kill-buffer-and-window)
-        (magithub-issue-view issue)))))
-
-(defun magithub-issue-wsubmit-pull-request (&rest _)
-  (interactive)
-  (let ((pull-request `((title  . ,(s-trim (magithub-issue--widget-value 'title)))
-                        (body   . ,(s-trim (magithub-issue--widget-value 'body)))
-                        (base   . ,(alist-get 'base magithub-issue--extra-data))
-                        (head   . ,(alist-get 'head magithub-issue--extra-data)))))
+  (let ((pull-request `(,@(magithub-issue-post--parse-buffer)
+                        (base . ,(alist-get 'base magithub-issue--extra-data))
+                        (head . ,(alist-get 'head magithub-issue--extra-data)))))
     (when (s-blank-p (alist-get 'title pull-request))
       (user-error "Title is required"))
     (when (yes-or-no-p "Are you sure you want to submit this pull request? ")
       (when (y-or-n-p "Allow maintainers to modify this pull request? ")
         (push (cons 'maintainer_can_modify t) pull-request))
-      (let ((pr (condition-case err
+      (let ((pr (condition-case _
                     (magithub-request
                      (ghubp-post-repos-owner-repo-pulls (magithub-repo) pull-request))
                   (ghub-422
                    (user-error "This pull request already exists!")))))
-        (kill-buffer-and-window)
         (magithub-issue-view pr)))))
-
-(defun magithub-issue-wcancel (&rest _)
-  (interactive)
-  ;; It'd be nice to have a sort of kill ring for cancelled issues
-  (when (yes-or-no-p "You will lose this buffer completely; are you sure? ")
-    (kill-buffer-and-window)))
 
 (provide 'magithub-issue-post)

RelNotes/0.1.6.org
@@ -7,6 +7,9 @@
 - If you were using ~magit-header-line~ to customize the appearance of
   the =Issues= and =Pull Requests= section headers, those now use the
   ~magit-section-heading~ face.  [[PR:196]]
+- Many functions related to issue/post creation have been reworked.
+  Instead of the widget framework, we now use =magithub-edit-mode=.  See
+  more details in 'New Features'.  [[PR:204]]
 
 * New Features
 - Browse commits by using =w= on a commit section.
@@ -22,6 +25,13 @@
   These symbols should allow other GitHub-sensitive packages to use
   the work Magithub has already done without depending on Magithub
   directly.  [[PR:201]]
+- The widget interface for writing issues and pull requests is gone!
+  Now, everything uses the framework debuted for writing comments.
+  For issues and pull requests, the first line (i.e., everything up to
+  the first newline character) is parsed as the title; everything else
+  as the body.  Now issues, pull requests, and comments use a common
+  interface that supports submitting, cancelling, and saving drafts to
+  finish later.  [[PR:204]]
 
 * Bug Fixes
 - In ~magithub-repo~, an API request is no longer made when the

magithub-comment.el
@@ -87,38 +87,6 @@
 (defun magithub-comment-source-repo (comment)
   (magithub-issue-repo (magithub-comment-source-issue comment)))
 
-(defun magithub-comment-draft-file (repo issue)
-  "Get the filepath of the comment draft for REPO/ISSUE."
-  (let-alist issue
-    (expand-file-name (format "%s-comment" .number)
-                      (magithub-repo-data-dir repo))))
-
-(defun magithub-comment-draft-save (repo issue comment)
-  "Save a draft reply to REPO/ISSUE as COMMENT."
-  (interactive (list (thing-at-point 'github-repository)
-                     (thing-at-point 'github-issue)
-                     (buffer-string)))
-  (make-directory (magithub-repo-data-dir repo) t)
-  (with-temp-buffer
-    (insert comment)
-    (write-file (magithub-comment-draft-file repo issue)))
-  (set-buffer-modified-p nil)
-  (message "Draft saved"))
-
-(defun magithub-comment-draft-load (repo issue)
-  "Load the draft reply to REPO/ISSUE."
-  (let ((file (magithub-comment-draft-file repo issue)))
-    (and (file-exists-p file)
-         (with-temp-buffer
-           (insert-file-contents file)
-           (buffer-string)))))
-
-(defun magithub-comment-draft-delete (repo issue)
-  "Delete the draft file for REPO/ISSUE if it exists."
-  (let ((f (magithub-comment-draft-file repo issue)))
-    (when (file-exists-p f)
-      (delete-file f magit-delete-by-moving-to-trash))))
-
 (defun magithub-comment-insert (comment)
   "Insert a single issue COMMENT."
   (let-alist comment
@@ -172,12 +140,6 @@ the comment; see `magithub-comment-view' and
         (select-window window))
       (kill-buffer buf))))
 
-(defvar magithub-comment-edit-map
-  (let ((m (make-sparse-keymap)))
-    (define-key m [remap save-buffer] #'magithub-comment-draft-save)
-    m)
-  "Local map for comment-edit buffers.")
-
 ;;;###autoload
 (defun magithub-comment-new (issue &optional discard-draft initial-content)
   "Comment on ISSUE in a new buffer.
@@ -191,36 +153,19 @@ initial contents of the reply if there is no draft."
                    (unless (derived-mode-p 'magithub-issue-view-mode)
                      (magithub-issue-view issue)))))
   (let* ((issueref (magithub-issue-reference issue))
-         (repo (magithub-issue-repo issue))
-         (draft (magithub-comment-draft-load repo issue)))
-    (when (and draft discard-draft)
-      (with-current-buffer (get-buffer-create " *draft*")
-        (insert draft)
-        (view-buffer-other-window (current-buffer))
-        (when (yes-or-no-p "Discard this draft? ")
-          (magithub-comment-draft-delete repo issue)
-          (setq draft nil))
-        (kill-buffer (current-buffer))))
-    (magithub-edit-new
-        (concat "reply to " issueref)
-        #'magithub-issue-comment-submit
-        #'magithub-issue-comment-cancel
-        magithub-comment-edit-map
-      (lambda ()
-        (setq-local magithub-issue issue)
-        (setq-local magithub-repo repo)
-        (magit-set-header-line-format
-         (substitute-command-keys
-          (format "replying to %s | %s | %s"
-                  issueref
-                  "submit: \\[magithub-edit-submit]"
-                  "cancel: \\[magithub-edit-cancel]")))
-        (if (and (null draft) initial-content)
-            (insert initial-content)
-          (when draft
-            (insert draft)
-            (message "Loaded existing draft")))
-        (goto-char (point-max))))))
+         (repo (magithub-issue-repo issue)))
+    (with-current-buffer
+        (magithub-edit-new (concat "reply to " issueref)
+          :header (concat "replying to " issueref)
+          :submit #'magithub-issue-comment-submit
+          :content initial-content
+          :prompt-discard-draft discard-draft
+          :file (let-alist issue
+                  (expand-file-name (format "%s-comment" .number)
+                                    (magithub-repo-data-dir repo))))
+      (setq-local magithub-issue issue)
+      (setq-local magithub-repo repo)
+      (magit-display-buffer (current-buffer)))))
 
 (defun magithub-comment-reply (comment &optional discard-draft issue)
   "Reply to COMMENT on ISSUE.
@@ -242,11 +187,6 @@ from COMMENT."
        (goto-char (point-max))
        (insert "\n\n")
        (buffer-string)))))
-
-(defun magithub-issue-comment-cancel ()
-  "Cancel current comment."
-  (interactive)
-  (call-interactively #'magithub-comment-draft-save))
 
 (defun magithub-issue-comment-submit (issue comment &optional repo)
   "On ISSUE, submit a new COMMENT.
@@ -274,9 +214,9 @@ not provided."
   ;; confirmed; submit the issue
   (magithub-request
    (ghubp-post-repos-owner-repo-issues-number-comments
-    repo issue `((body . ,comment))))
-  (message "Success")
-  (magithub-comment-draft-delete repo issue))
+       repo issue `((body . ,comment))))
+  (magithub-edit-delete-draft)
+  (message "Success"))
 
 (provide 'magithub-comment)
 ;;; magithub-comment.el ends here

magithub-edit-mode.el
@@ -32,15 +32,19 @@
   (let ((m (make-sparse-keymap)))
     (define-key m (kbd "C-c C-c") #'magithub-edit-submit)
     (define-key m (kbd "C-c C-k") #'magithub-edit-cancel)
-    m))
+    m)
+  "Keymap for `magithub-edit-mode'.")
 
 ;;;###autoload
 (define-derived-mode magithub-edit-mode gfm-mode "Magithub-Edit"
   "Major mode for editing Github issues and pull requests.")
 
-(defvar-local magithub-edit-submit-function nil)
-(defvar-local magithub-edit-cancel-function nil)
-(defvar-local magithub-edit-previous-buffer nil)
+(defvar-local magithub-edit-submit-function nil
+  "Populated by SUBMIT in `magithub-edit-new'.")
+(defvar-local magithub-edit-cancel-function nil
+  "Populated by CANCEL in `magithub-edit-new'.")
+(defvar-local magithub-edit-previous-buffer nil
+  "The buffer we were in when the edit was initiated.")
 
 (defface magithub-edit-title
   '((t :inherit markdown-header-face-1))
@@ -48,36 +52,151 @@
   :group 'magithub-faces)
 
 (defun magithub-edit-submit ()
+  "Submit this post.
+Uses `magithub-edit-submit-function' to do so."
   (interactive)
+  (unless (commandp magithub-edit-submit-function t)
+    (error "No submit function defined"))
   (magithub-edit--done magithub-edit-submit-function)
   (magithub-cache-without-cache t
     (magit-refresh-buffer)))
+
 (defun magithub-edit-cancel ()
+  "Cancel this post.
+Offer to save a draft if the buffer is considered modified, then
+call `magithub-edit-cancel-function'."
   (interactive)
+  ;; Offer to save the draft
+  (if (and (buffer-modified-p)
+           (y-or-n-p "Save draft? "))
+      (save-buffer)
+    (set-buffer-modified-p nil))
+
+  ;; If the saved draft is empty, might as well delete it
+  (when (and (stringp buffer-file-name)
+             (file-readable-p buffer-file-name)
+             (string= "" (let ((f buffer-file-name))
+                           (with-temp-buffer
+                             (insert-file-contents f)
+                             (buffer-string)))))
+    (magithub-edit-delete-draft))
+
   (magithub-edit--done magithub-edit-cancel-function))
+
 (defun magithub-edit--done (callback)
-  (let ((prevbuf magithub-edit-previous-buffer))
-    (save-excursion
-      (when-let ((newbuf (call-interactively callback)))
+  "Cleanup this buffer.
+If CALLBACK is a command, call it interactively.  (This will
+usually be the SUBMIT or CANCEL commands from
+`magithub-edit-new'.)  If that function returns a buffer, switch
+to that buffer."
+  (let ((nextbuf magithub-edit-previous-buffer))
+    (when (commandp callback t)
+      (let ((newbuf (save-excursion
+                      (call-interactively callback))))
         (when (bufferp newbuf)
-          (switch-to-buffer newbuf))))
+          (setq nextbuf newbuf))))
+    (set-buffer-modified-p nil)
     (kill-buffer)
-    (when prevbuf
+    (when nextbuf
       (let ((switch-to-buffer-preserve-window-point t))
-        (switch-to-buffer prevbuf)))))
-(defun magithub-edit-new (buffer-name submit-func cancel-func local-map setup)
-  (declare (indent 4))
-  (let ((prevbuf (current-buffer)))
+        (switch-to-buffer nextbuf)))))
+
+(defun magithub-edit-delete-draft ()
+  "Delete the draft for the current edit buffer."
+  (when (file-exists-p buffer-file-name)
+    (delete-file buffer-file-name magit-delete-by-moving-to-trash))
+  (message "Deleted %s" buffer-file-name)
+  (set-visited-file-name nil))
+
+(cl-defun magithub-edit-new (buffer-name &key cancel content file header prompt-discard-draft submit template)
+  "Generate a new edit buffer called BUFFER-NAME and return it.
+'Edit' buffers provide a common interface and handling for
+submitting, cancelling, and saving drafts of posts.
+
+CANCEL is a function to use upon \\[magithub-edit-cancel].
+
+CONTENT is initial content for the buffer.  It is considered
+novel and the buffer will not be closed without prompting to save
+a draft.
+
+FILE is the file to use for drafts of this post.
+
+HEADER is a title to use in the header line of the new buffer.
+
+If PROMPT-DISCARD-DRAFT is non-nil, this function will display
+the draft before offering to delete it.  This option is
+recommended when using \\[universal-argument] with the command
+that calls this function.
+
+SUBMIT is a function to use upon \\[magithub-edit-submit].
+
+TEMPLATE is like CONTENT, but is not considered novel.  We won't
+ask to save a draft here if post is cancelled."
+  (declare (indent 1))
+  (let ((prevbuf (current-buffer))
+        (file (and (stringp file)
+                   (file-writable-p file)
+                   file))
+        draft)
+
+    ;; Load the draft
+    (setq draft (and (stringp file)
+                     (file-readable-p file)
+                     (with-temp-buffer
+                       (insert-file-contents file)
+                       (buffer-string))))
+    (when (string= draft "")
+      (setq draft nil))
+
+    ;; Discard the draft if desired
+    (when (and draft prompt-discard-draft)
+      (with-current-buffer (get-buffer-create " *draft*")
+        (erase-buffer)
+        (insert draft)
+        (view-buffer-other-window (current-buffer))
+        (when (yes-or-no-p "Discard this draft? ")
+          (setq draft nil)
+          (when (file-writable-p file)
+            (delete-file file magit-delete-by-moving-to-trash)))
+        (kill-buffer (current-buffer))))
+
     (with-current-buffer (get-buffer-create buffer-name)
       (magithub-edit-mode)
-      (use-local-map (let ((m local-map))
-                       (set-keymap-parent m magithub-edit-mode-map)
-                       m))
-      (funcall setup)
+
       (setq magithub-edit-previous-buffer prevbuf
-            magithub-edit-submit-function submit-func
-            magithub-edit-cancel-function cancel-func)
-      (switch-to-buffer-other-window (current-buffer)))))
+            magithub-edit-submit-function submit
+            magithub-edit-cancel-function cancel)
+      (magit-set-header-line-format
+       (substitute-command-keys
+        (let ((line "submit: \\[magithub-edit-submit] | cancel: \\[magithub-edit-cancel]"))
+          (when header
+            (setq line (concat line " | " header)))
+          line)))
+
+      (when file
+        (let ((orig-name (buffer-name))
+              (dir default-directory))
+          (set-visited-file-name file)
+          (rename-buffer orig-name)
+          (cd dir)))
+
+      (cond
+       (draft
+        (insert draft)
+        (set-buffer-modified-p nil)
+        (goto-char (point-max))
+        (message "Loaded existing draft from %s" file))
+       (content
+        (insert content)
+        (goto-char (point-max))
+        (message "Loaded initial content"))
+       (template
+        (insert template)
+        (set-buffer-modified-p nil)
+        (goto-char (point-min))
+        (message "Loaded template")))
+
+      (current-buffer))))
 
 (provide 'magithub-edit-mode)
 ;;; magithub-edit-mode.el ends here

commit 82fb36699ea973ca19e527480ac5677bdda31745

Posts without newlines now keep the last character of the title
Caused by 2a372fbc16a8f6f033be7da951c7ec0b2ffd15be

magithub-issue-post.el
@@ -22,15 +22,9 @@
         (magithub-issue-view issue)))))
 
 (defun magithub-issue-post--parse-buffer ()
-  (let (split)
-    (save-restriction
-      (widen)
-      (save-excursion
-        (goto-char (point-min))
-        (forward-line)
-        (setq split (point)))
-      `((title . ,(s-trim (buffer-substring-no-properties (point-min) (1- split))))
-        (body  . ,(s-trim (buffer-substring-no-properties split (point-max))))))))
+  (let ((lines (split-string (buffer-string) "\n")))
+    `((title . ,(s-trim (car lines)))
+      (body . ,(s-trim (mapconcat #'identity (cdr lines) "\n"))))))
 
 (defun magithub-issue-new (repo)
   (interactive (list (magithub-repo)))