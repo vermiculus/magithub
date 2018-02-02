vermiculus/magithub

commit f09e5ce9ec3db975cfefba41ee6949ddf3616300

Only delete draft if it exists

magithub-edit-mode.el
@@ -103,9 +103,10 @@ to that buffer."
 
 (defun magithub-edit-delete-draft ()
   "Delete the draft for the current edit buffer."
-  (when (file-exists-p buffer-file-name)
-    (delete-file buffer-file-name magit-delete-by-moving-to-trash))
-  (message "Deleted %s" buffer-file-name)
+  (when (and (stringp buffer-file-name)
+             (file-writable-p buffer-file-name))
+    (delete-file buffer-file-name magit-delete-by-moving-to-trash)
+    (message "Deleted %s" buffer-file-name))
   (set-visited-file-name nil))
 
 (cl-defun magithub-edit-new (buffer-name &key cancel content file header prompt-discard-draft submit template)

commit 98067e35626d092bb829bc8c218fec44f04c3023

Only delete the file if it exists (again)
This was semi-broken in #249.

magithub-edit-mode.el
@@ -104,6 +104,7 @@ to that buffer."
 (defun magithub-edit-delete-draft ()
   "Delete the draft for the current edit buffer."
   (when (and (stringp buffer-file-name)
+             (file-exists-p buffer-file-name)
              (file-writable-p buffer-file-name))
     (delete-file buffer-file-name magit-delete-by-moving-to-trash)
     (message "Deleted %s" buffer-file-name))