7c151da9643da23f62f0619c02895b7a974c047d
Jonas Bernoulli <jonas@bernoul.li>
2018-03-19 17:30:35 +0100

    Add library headers and footers

--- magithub-issue-post.el
+++ magithub-issue-post.el
@@ -1,3 +1,26 @@
+;;; magithub-issue-post.el ---                   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017-2018  Sean Allred
+
+;; Author: Sean Allred <code@seanallred.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'magithub-core)
 (require 'magithub-issue)
 (require 'magithub-label)
@@ -195,3 +218,4 @@ See also URL
       (magithub-issue-view pr))))
 
 (provide 'magithub-issue-post)
+;;; magithub-issue-post.el ends here
